69858770eb18c2d608660e1e922a5103fd64b395
shivam <shivamrajpoot3703@gmail.com>
2023-10-19 14:59:56 +0530

    exercise

---
+++ block-BNaaai/index.js
@@ -0,0 +1,23 @@
+var url = require("url");
+
+const parsedUrl = url.parse(
+  "https://blog.altcampus.io:80/students/register?name=altcampus&gender=male",
+  true
+);
+console.log(parsedUrl)
+
+// console parsedUrl.pathname
+console.log(parsedUrl.pathname)
+
+// console parsedUrl.query
+console.log(parsedUrl.query)
+
+
+// console.log parsed Url host and protocol
+console.log(parsedUrl.host,'and',parsedUrl.protocol)
+
+
+// Do a request on google.com and view headers inside developer's network tab by clicking on a specific request.
+
+const parsedGoogleUrl = url.parse("google.com");
+console.log(parsedGoogleUrl);
---
+++ block-BNaaai/index.md
@@ -0,0 +1,15 @@
+Use URL module to parse url informaton.
+
+// create file index.js
+var url = require("url");
+const parsedUrl = url.parse(
+  "https://blog.altcampus.io:80/students/register?name=altcampus&gender=male",
+  true
+);
+
+console.log(parsedUrl); 
+console parsedUrl.pathname // students/register
+console parsedUrl.query // name=altcampus&gender=male
+console.log parsed Url host and protocol //blog.altcampus.io:80 and https
+
+Do a request on google.com and view headers inside developer's network tab by clicking on a specific request.
